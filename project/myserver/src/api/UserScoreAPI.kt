f64471cbf11489d67a7de0c57920d7931ebfd880
YungHsin <yhchang0921@gmail.com>
2021-06-20 17:39:18 +0800

    可執行專案

---
+++ project/myserver/src/api/UserScoreAPI.kt
@@ -0,0 +1,28 @@
+package yaya.idv.api
+
+import com.google.gson.Gson
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import yaya.idv.model.UserScore
+import yaya.idv.service.UserScoreService
+
+fun Route.UserScoreAPI() {
+
+    post("/uploadScore") {//上傳分數API
+        var data = Gson().fromJson(call.receive<String>(), UserScore::class.java)
+        if (data == null) {
+            call.respond(HttpStatusCode.BadRequest, "玩家資料不存在")
+        }else {
+            UserScoreService().saveScore(data)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+
+    get("/getRankList") {//取得排行榜API
+        call.respond(HttpStatusCode.OK, UserScoreService().getRankList())
+    }
+
+}
---
+++ project/myserver/src/model/UserScore.kt
@@ -0,0 +1,3 @@
+package yaya.idv.model
+
+data class UserScore(var id:Long?=null, var user:String, var score:Int)
