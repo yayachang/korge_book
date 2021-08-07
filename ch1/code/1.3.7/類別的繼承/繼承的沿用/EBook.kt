10db11cd06b66f5320ab45e59c044925bfd4113f
YungHsin <yhchang0921@gmail.com>
2021-08-07 16:23:36 +0800

    Update EBook.kt

--- ch1/code/1.3.7/類別的繼承/繼承的沿用/EBook.kt
+++ ch1/code/1.3.7/類別的繼承/繼承的沿用/EBook.kt
@@ -1,5 +1,5 @@
 class EBook:Book(){
-    override var name ="${ super.name}(電子版)"
+    override var name ="${super.name}(電子版)"
     override fun calculatePrice():Int{
         return (super.calculatePrice()*0.9).toInt()
     }
